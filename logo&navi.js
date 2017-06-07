8cfdfd4696713522e7327e60e18d3b5b69fc2b96
Manny Flores <mfloramirez11@gmail.com>
2017-06-06 21:52:04 -0700

    broken but editing

---
+++ logo&navi.js
@@ -0,0 +1,25 @@
+ document.getElementById("logo&navi").innerHTML =
+
+
+
+'<div id="bannerimage">' +
+    '<div class="inner">' +
+        '<!-- If logo is update please make sure it is at 1000 x 200 -->' +
+        '<img src="Logos/SHPE%20SBLA%20Official%20Logo.jpg" width="100%" height="auto" />' + 
+    '</div>' +
+'</div>' +
+    
+'<!-- 2. Div for the navigation bar -->' +
+'<div id="menu">' +
+    '<ul>' +
+        '<li class="current_page_item"><a href="/index.html" accesskey="1" title="">Home</a></li>' +
+        '<li><a href="/Pages/HTML/history.html" accesskey="2" title="">History</a></li>' +
+        '<li><a href="/Pages/HTML/calendar.html" accesskey="3" title="">Calendar</a></li>' +
+        '<li><a href="//Pages/HTML/leadershpe.html" accesskey="4" title="">Our LeaderSHPE</a></li>' +
+        '<li><a href="/Pages/HTML/studentchapters.html" accesskey="5" title="">Student Chapters</a></li>' +
+        '<li><a href="/Pages/HTML/resources.html" accesskey="6" title="">Resources</a></li>' +
+        '<li><a href="/Pages/HTML/howtojoin.html" accesskey="7" title="">How To Join</a></li>' +
+        '<li><a href="/Pages/HTML/connect.html" accesskey="8" title="">Connect</a></li>' +
+        '<li><a href="/Pages/HTML/store.html" accesskey="8" title="">Store</a></li>' +
+    '</ul>' +
+'</div>';
