21c782992b21086b34b7b2a0fabd539d890a7bb6
Manny Flores <mfloramirez11@gmail.com>
2017-07-01 16:01:16 -0700

    semi

--- logonnavi.js
+++ logonnavi.js
@@ -9,13 +9,13 @@
 '<div id="menu">' +
     '<ul>' +
         '<li class="current_page_item"><a href="/index.html" accesskey="1" title="">Home</a></li>' +
-        '<li><a href="/Pages/HTML/history.html" accesskey="2" title="">History</a></li>' +
-        '<li><a href="/Pages/HTML/calendar.html" accesskey="3" title="">Calendar</a></li>' +
-        '<li><a href="//Pages/HTML/leadershpe.html" accesskey="4" title="">Our LeaderSHPE</a></li>' +
-        '<li><a href="/Pages/HTML/studentchapters.html" accesskey="5" title="">Student Chapters</a></li>' +
-        '<li><a href="/Pages/HTML/resources.html" accesskey="6" title="">Resources</a></li>' +
-        '<li><a href="/Pages/HTML/howtojoin.html" accesskey="7" title="">How To Join</a></li>' +
-        '<li><a href="/Pages/HTML/connect.html" accesskey="8" title="">Connect</a></li>' +
-        '<li><a href="/Pages/HTML/store.html" accesskey="8" title="">Store</a></li>' +
+        '<li><a href="/history.html" accesskey="2" title="">History</a></li>' +
+        '<li><a href="/calendar.html" accesskey="3" title="">Calendar</a></li>' +
+        '<li><a href="/leadershpe.html" accesskey="4" title="">Our LeaderSHPE</a></li>' +
+        '<li><a href="/studentchapters.html" accesskey="5" title="">Student Chapters</a></li>' +
+        '<li><a href="/resources.html" accesskey="6" title="">Resources</a></li>' +
+        '<li><a href="/howtojoin.html" accesskey="7" title="">How To Join</a></li>' +
+        '<li><a href="/connect.html" accesskey="8" title="">Connect</a></li>' +
+        '<li><a href="/store.html" accesskey="8" title="">Store</a></li>' +
     '</ul>' +
 '</div>';
